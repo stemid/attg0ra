284ce00f010ebe2ab590e859f1ac5f84f187018f
Stefan Midjich <swehack@gmail.com>
2013-12-10 23:55:47 +0100

    ui-bootstrap för modal och en create mall för modalen.

--- static/assets/js/controllers.js
+++ static/assets/js/controllers.js
@@ -1,16 +1,30 @@
 'use strict';
 
+// Här skapas objektet för att göra nya controllers med. 
 var todoAppControllers = angular.module('todoAppControllers', []);
 
-todoAppControllers.controller('todoListCtrl', ['$scope', '$http', 
-  function TodoListCtrl($scope, $http) {
+// Här används det skapade objektet för att göra en controller. 
+// I controllern ska man definiera hur data ska hanteras, var data kommer ifrån.
+// Till exempel här där data hämtas från en webbserver och stoppas in i $scope
+// för att göras tillgängligt till en mall som använder sig av controllern. 
+todoAppControllers.controller('todoListCtrl', 
+                              [ // Här listas beroenden innan själva funktionskoden. 
+                                '$scope', 
+                                '$http', 
+                                '$log',
+                                '$modal',
+
+  function TodoListCtrl($scope, $http, $log, $modal) {
+    // Här hämtas JSON data från en webbserver med $http tjänsten, som är definierad 
+    // i början som ett beroende för controllern. 
     $http.get('http://localhost:8000/').
-      success(function (data) {
-        console.log(data);
+      success(function (data, status) {
+        $log.info('HTTP GET returned: ' + data + ', status: ' + status);
         $scope.todos = data;
       }
     ).
-      error(function (response) {
+      error(function (data, status, headers, config) {
+        $log.info('HTTP GET returned error: ' + status);
         $scope.todos = [
           {
             'title': 'No items found'
@@ -18,8 +32,41 @@ todoAppControllers.controller('todoListCtrl', ['$scope', '$http',
         ];
       }
     );
+    
+    // Här kan även definieras allt möjligt annat, som t.ex. en modal med ett formulär 
+    // som ska visas i samma controller
+    $scope.open = function () {
+      var createFormModal = $modal.open({
+        templateUrl: 'create.html',
+        controller: 'ModalCreateCtrl',
+        resolve: {
+          todo: function () {
+            $log.info('resolving');
+            return $scope.todo;
+          }
+        }
+      });
+    }
   }
 ]);
+
+// Här definieras modal controllern som anropas från TodoListCtrl.
+// En controller är i sin enklaste form bara en funktion med ett visst symbolnamn
+// som går att slå upp i någon sorts JS-namnrymd. 
+var ModalCreateCtrl = function ($scope, $modalInstance, $log) {
+  $scope.empty = {};
+
+  $log.info('in the modal controller');
+
+  $scope.create = function (input) {
+    var $form = $('#create-form');
+    var data = $form.serialize();
+    $http.post('http://localhost:8000/', data).
+      success(function (data) {
+        $log.info(data);
+      });
+  };
+}
 
 todoAppControllers.controller('todoShowCtrl', ['$scope', '$routeParams',
   function ($scope, $routeParams) {
--- static/assets/js/todo.js
+++ static/assets/js/todo.js
@@ -1,10 +1,21 @@
 'use strict';
 
+// Detta skapar den grundläggande applikationen som allt annat ska byggas på.
+// Här anges namnet på applikationen som sedan skapar objektet todoApp. 
+// Samt beroenden till applikationen, som i detta fallet är ngRoute för att 
+// kunna definiera routes, och alla controllers från controllers.js. 
 var todoApp = angular.module('todoApp', [
   'ngRoute',
-  'todoAppControllers'
+  'todoAppControllers',
+  'ui.bootstrap'
 ]);
 
+// Här definieras routes precis som i bottle.py t.ex.
+// Det verkar som att man använder when och otherwise metoderna i routeProvider 
+// objektet, och varje gång en av metoderna anropas så får man en ny instans 
+// av routeProvider objektet tillbaka. 
+// Varje anrop som ska använda sig av någon sorts dynamisk data måste även ange
+// en controller som ska tillhandahålla datan. 
 todoApp.config(['$routeProvider',
   function ($routeProvider) {
     $routeProvider.
